721648a9d8aa288f3ff4c116458fe353fe711ebf
Ruslan Rodionov <rod1ionovvv@gmail.com>
2023-01-08 00:36:42 +0300

    frontend update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import {Component} from "react";
 import {connect, sendMsg} from "./api";
+import Header from "./components/Header";
+import ChatHistory from "./components/ChatHistory";
 
 class App extends Component {
-  constructor(props) {
+
+    componentDidMount() {
+        connect((msg) => {
+            console.log("New Message")
+            this.setState(prevState => ({
+                chatHistory: [...this.state.chatHistory, msg]
+            }))
+            console.log(this.state);
+        });
+    }
+
+    constructor(props) {
     super(props);
-    connect();
+    this.state = {
+        chatHistory: []
+    }
   }
 
   send() {
@@ -16,6 +31,8 @@ class App extends Component {
   render() {
     return (
         <div className="App">
+            <Header />
+            <ChatHistory chatHistory={this.state.chatHistory} />
           <button onClick={this.send}>Hit</button>
         </div>
     );
